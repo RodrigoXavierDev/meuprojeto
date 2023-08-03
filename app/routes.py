67416c04fffbd901dff2a2a151c808e26025d4f4
Versalhes31 <xavierlrodrigo@gmail.com>
2023-08-03 10:30:42 -0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template('index.html',titulo = "Pagina inicial")
+
+@app.route('/contatos')
+def contatos():
+    return render_template("contatos.html",titulo = "Contatos")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html",titulo = "Sobre")
+
