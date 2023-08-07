c72e6cb80a0bd3e24eaff41ad7b5a54bb03d84d4
Versalhes31 <xavierlrodrigo@gmail.com>
2023-08-07 11:51:56 -0300

    fiz um monte de coisa

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,EmailField,TelField,TextAreaField,SubmitField
+from wtforms.validators import DataRequired
+
+class Contato(FlaskForm):
+    nome = StringField('nome', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired()])
+    telefone = TelField('telefone', validators=[DataRequired()])
+    mensagem = TextAreaField('mensagem')
+    enviar = SubmitField('enviar')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,32 @@
 from app import app
 from flask import render_template
+from app.forms import Contato
 
 @app.route('/')
 def index():
     return render_template('index.html',titulo = 'Pagina inicial')
 
-@app.route('/contatos')
+@app.route('/contatos', methods=['POST', 'GET'])
 def contatos():
-    return render_template('contatos.html',titulo = 'Contatos')
+    dados_formulario = None
+    formulario = Contato()
+    print('Acessou a rota contatos')
+    if formulario.validate_on_submit():
+        print('o formulario foi validado')
+        nome = formulario.nome.data
+        email = formulario.email.data
+        telefone = formulario.telefone.data
+        mensagem = formulario.mensagem.data
+    dados_formulario = {
+        'nome': nome,
+        'email': email,
+        'telefone': telefone,
+        'mensagem': mensagem
+    }
+
+
+    return render_template('contatos.html',titulo = 'Contatos', formulario = formulario,dados_formulario = formulario)
+
 
 @app.route('/sobre')
 def sobre():
